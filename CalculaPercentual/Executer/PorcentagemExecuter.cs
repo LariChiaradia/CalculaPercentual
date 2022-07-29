6eaa3e88d4456d1e8138b34f21157d1b05f2c268
Larissa Chiaradia <lari.chiaradia.silva@gmail.com>
2022-07-29 00:31:54 -0300

    feat: add calculo de percentual.

---
+++ CalculaPercentual/Executer/PorcentagemExecuter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaPercentual.Executer
+{
+    public static class PorcentagemExecuter
+    {
+        public static double Porcentagem(double valor, double total)
+        {
+            return (valor * 100) / total;
+        }
+    }
+
+
+}
--- CalculaPercentual/Program.cs
+++ CalculaPercentual/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculaPercentual.Executer;
+using CalculaPercentual.Models;
+
+var estados = new List<Estado>
+{
+    new()
+    {
+        Name = "SP",
+        Valor = 67836.43
+    },
+    new()
+    {
+        Name = "RJ",
+        Valor = 36678.66
+    },
+    new()
+    {
+        Name = "MG",
+        Valor = 29229.88
+    },
+    new()
+    {
+        Name = "ES",
+        Valor = 27165.48
+    },
+    new()
+    {
+        Name = "Outros",
+        Valor = 19849.53
+    }
+};
+
+var total = estados.Sum(x => x.Valor);
+
+foreach (var estado in estados)
+{
+    var porcentagem = PorcentagemExecuter.Porcentagem(estado.Valor, total);
+    Console.WriteLine($"Estado : {estado.Name} Porcentagem: %{porcentagem}");
+}
